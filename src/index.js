c1214db0c8f398e0084ed5df84c81c4451a8fc35
Brosner522 <rosnerbb@gmail.com>
2021-04-09 15:26:09 -0400

    First event listener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+
+document.addEventListener("DOMContentLoaded", (evt) => {
+  evt.preventDefault()
+  create-task-form.addEventListener("Submit", (evt) => {
+    evt.preventDefault()
+    
+    let form = evt.target 
+    let input = form.item 
+    let userTypes = input.value
+
+    
+  });
 });
+
+
+
